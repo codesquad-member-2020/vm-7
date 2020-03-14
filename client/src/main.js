c6e0207d6e5e037fafa2a51bfafa9ed9b8e51ad7
Hu2y <sanghun_@daum.net>
2020-03-15 01:57:35 +0900

    [add] main.js 파일 초기 세팅

---
+++ client/src/main.js
@@ -0,0 +1,48 @@
+import Controller from './controller/Controller.js'
+import UserModel from './models/UserModel.js'
+import ProductModel from './models/ProductModel.js'
+import ProductSelectView from './views/ProductSelectView.js'
+import ProductView from './views/ProductView.js'
+import WalletView from './views/WalletView.js'
+import ProductComponent from './components/productComponent.js'
+import InputCashComponent from './components/inputCashComponent.js'
+import SelectNumberComponent from './components/selectNumberComponent.js'
+import LogComponent from './components/logComponent.js'
+import WalletInfoComponent from './components/walletInfoComponent.js'
+import TotalCashComponent from './components/totalCashComponent.js'
+import {$} from './utils/index.js'
+
+// view
+const productView = new ProductView({
+  productComponent: new ProductComponent()
+})
+const productSelectView = new ProductSelectView({
+  inputCashComponent: new InputCashComponent(),
+  selectNumberComponent: new SelectNumberComponent(),
+  logComponent: new LogComponent()
+})
+const walletView = new WalletView({
+  walletInfoComponent: new WalletInfoComponent(),
+  totalCashComponent: new TotalCashComponent(),
+  walletViewArea: $('.wallet-view')
+})
+
+// model
+const productModel = new ProductModel({
+  productView
+})
+const userModel = new UserModel({
+  productSelectView,
+  walletView
+})
+
+// controller
+const controller = new Controller({
+  model : { productModel, userModel },
+  view: { productView, productSelectView, walletView }
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    controller.initialize()
+  }
+)
