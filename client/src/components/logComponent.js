2bd02bbf0ae5ed049ebbcb77fd1c0c1371d83b06
Hu2y <sanghun_@daum.net>
2020-03-14 23:11:30 +0900

    [add] 컴포넌트 렌더링 작업 inputCashComponent logComponent selectNumberComponent 컴포넌트 작성

---
+++ client/src/components/logComponent.js
@@ -0,0 +1,15 @@
+import {$} from '../utils/index.js'
+
+class LogComponent {
+  constructor() {
+  }
+  render(data= '') {
+    return `
+    <div class="select-production">
+      <textarea name="" id="" cols="34" rows="9">${data}</textarea>
+    </div>
+    `
+  }
+}
+
+export default LogComponent
