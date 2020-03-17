e7650bf2217087d22f687c06e8a9c590cd076b13
Hu2y <sanghun_@daum.net>
2020-03-18 00:33:34 +0900

    [add] 옵저버 이벤트에 따른 데이터 조작 및 전달 1. 초기 walletView, selectView 렌더링 작업 2. 지갑 버튼이 눌려졌을때 해당되는 금액수 조작 3. 지갑 금액수에 따른 현재 토탈금액 계산 4. 투입된 금액 계산 5. 바뀐 데이터들을 구독하고있는 컴포넌트들에게 전달

--- client/src/models/UserModel.js
+++ client/src/models/UserModel.js
@@ -1,18 +1,58 @@
-class ProductModel {
-  constructor({productSelectView, walletView, USER_URL}) {
-    this.productSelectView = productSelectView,
-    this.walletView = walletView
+import Observer from '../utils/observe.js'
+
+class UserModel extends Observer {
+  constructor({ USER_URL }) {
+    super()
     this.WALLET_URL = USER_URL
     this.walletData = {}
+    this.expense = {}
   }
 
   async fetchData() {
-    this.productSelectView.initRender()
     const res = await fetch(this.WALLET_URL)
     const walletData = await res.json()
     this.walletData = walletData
-    this.walletView.initRender(walletData)
+    this.initExpense()
+    this.fireEvent("initWalletViewRender", walletData);
+    this.fireEvent("initSelectViewRender", walletData);
+  }
+
+  initExpense() {
+    this.expense = this.walletData.map(data=> ({...data}));
+    this.expense.forEach(init => init.number = 0);
+  }
+
+  computedTotalPrice(targetData) {
+    const totalPrice = targetData
+      .reduce((price, data) => 
+        price += parseInt(data.value) * parseInt(data.number), 0);
+
+    return totalPrice;
+  }
+
+  changeExpenseInfo({target: {value}}) {
+    this.expense.filter(targetValue => 
+        targetValue.value == value)
+      .map(data => {
+        data.number += 1;
+    });
+    const expenseTotal = this.computedTotalPrice(this.expense)
+    this.fireEvent("changeExpenseData", expenseTotal)
+  }
+
+  changeWalletInfo({target: {value}}) {
+    const targetData = this.walletData
+      .filter(targetValue => 
+        targetValue.value == value)
+      .map(data => {
+        if (data.number > 0 ) data.number -= 1;
+      return data
+    });
+    this.fireEvent("changeWalletData", ...targetData);
+
+    const totalPirce = this.computedTotalPrice(this.walletData);
+    this.fireEvent("totalWalletData", totalPirce);
   }
 }
 
-export default ProductModel
+export default UserModel
