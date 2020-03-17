1a83a817a098669c6d4576dad3d337eb8fe8e15e
Hu2y <sanghun_@daum.net>
2020-03-18 00:39:59 +0900

    [add] walletInfoComponent 옵저버 등록, 지갑금액버튼 이벤트 등록  - 지갑금액 버튼 이벤트 델리게이션  - 지갑금액 버튼을 누르면 usermodel에게 알림  - usermodel에서 이벤트에 따른 데이터를 가공하여 변화된 값을 재 렌더링

--- client/src/components/walletInfoComponent.js
+++ client/src/components/walletInfoComponent.js
@@ -1,7 +1,11 @@
-class TotalCashComponent {
-  constructor(data) {
-    
+import {$} from '../utils/index.js'
+
+class WalletInfoComponent {
+  constructor({ userModel }) {
+    this.userModel = userModel
+    this.registerObserver()
   }
+
   render(data) {
     const cashList = data.reduce((list, cash) => {
       list += 
@@ -12,11 +16,31 @@ class TotalCashComponent {
         return list
     }, "")
     return `
-    <ul>
+    <ul class="wallet-list">
       ${cashList}
     </ul>
     `
   }
+
+  eventHandler() {
+    $('.wallet-view').addEventListener("click", this.eventWalletbutton.bind(this));
+  }
+
+  eventWalletbutton(e) {
+    if(e.target.tagName !== "BUTTON") return
+    if(e.target.nextElementSibling.innerText == 0) return
+    this.userModel.changeWalletInfo(e);
+    this.userModel.changeExpenseInfo(e)
+  }
+
+  registerObserver() {
+    this.userModel.addEvent("changeWalletData", this.changeRenderWalletInfo.bind(this));
+  }
+
+  changeRenderWalletInfo(data) {
+    const targetElement = $(`button[value="${data.value}"]`);
+    targetElement.nextElementSibling.innerText = `${data.number}`
+  }
 }
 
-export default TotalCashComponent
+export default WalletInfoComponent
