30b58ab39af3fdd60c3ce65874fd85567419b3b8
Hu2y <sanghun_@daum.net>
2020-03-18 00:42:45 +0900

    [add] inputCashComponent 옵저버 등록 및 렌더링 함수 추가  - walletInfoComponent 조작으로 투입된 금액을 모델에서 조작하여 해당 컴포넌트에서 렌더링

--- client/src/components/inputCashComponent.js
+++ client/src/components/inputCashComponent.js
@@ -1,8 +1,9 @@
 import {$} from '../utils/index.js'
 
 class InputNumberComponent {
-  constructor() {
-    // $('.production-select-view').innerHTML = this.render();
+  constructor({userModel}) {
+    this.userModel = userModel
+    this.registerObserver()
   }
   render(data = 0) {
     return `
@@ -11,6 +12,14 @@ class InputNumberComponent {
           투입 금액 : <span class="price">${data}</span><span>원</span>
       </div>
     </div>`
+  }
+
+  registerObserver() {
+    this.userModel.addEvent("changeExpenseData", this.changeRenderExpense.bind(this))
+  }
+
+  changeRenderExpense(expenseTotal) {
+    $('.price').innerText = expenseTotal
   }
 }
 
