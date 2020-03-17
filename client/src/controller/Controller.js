dce6dec12873dc180c603d8568e1ed9327362d15
Hu2y <sanghun_@daum.net>
2020-03-18 00:37:01 +0900

    [add] 컨트롤러에 컴포넌트들 주입, 컴포넌트 이벤트 등록

--- client/src/controller/Controller.js
+++ client/src/controller/Controller.js
@@ -3,18 +3,21 @@ import Observer from '../utils/observe.js'
 class Controller {
   constructor({
     model: { productModel, userModel },
-    view: { productView, productSelectView, walletView }
+    view: { productView, productSelectView, walletView },
+    component: {selectNumberComponent, walletInfoComponent}
   }) {
     this.productModel = productModel
     this.userModel = userModel
     this.productView = productView
     this.productSelectView = productSelectView
     this.walletView = walletView
+    this.selectNumberComponent = selectNumberComponent
+    this.walletInfoComponent = walletInfoComponent
   }
 
   initialize () {
-    this.addObserveEvents();
     this.fetchProductData();
+    this.componentEventHandler()
   }
 
   // 초기 랜더링
@@ -23,14 +26,10 @@ class Controller {
     this.userModel.fetchData()
   }
 
-  // 옵저버들을 등록해야할듯..
-  addObserveEvents(){
-    // this.model.addEvent("onGetMemberListComplete", this.printMemberList.bind(this));
-  }
-
-  printMemberList(data){
-    // this.view.render(data);
-    
+  // component 핸들러
+  componentEventHandler() {
+    this.selectNumberComponent.eventHandler()
+    this.walletInfoComponent.eventHandler()
   }
 }
 
